14e9641f5f7f1fc928925cd16b5e70067eeb3e9a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-29 22:04:42 -0300

    Funções em Python

---
+++ Python/Fibonacci/sequncia_de_fibonacci.py
@@ -0,0 +1,23 @@
+'''fibonacci = [0,1]
+
+n = 10
+
+for i in range(2, n):
+        fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
+
+print(fibonacci)'''
+
+'''fibonacci = [0, 1]
+
+a = 0
+b = 1
+n = 2
+
+while n < 10:
+        n += 1
+        c = a + b
+        fibonacci.append(c)
+        a = b
+        b = c
+
+print(fibonacci)'''
---
+++ Python/funcoes/aula2904.py
@@ -0,0 +1,70 @@
+'''def saudacao():
+    print(f'Olá Alunos feiosos e nóias!')
+
+saudacao()'''
+
+'''def saudacao(nome):
+    return (f'Olá {nome} feioso e nóia!')
+
+saudacao("Celso")
+
+mensage = saudacao('Celso')
+
+print(mensage)'''
+
+'''def calcular_media(nota1, nota2):
+    media = (nota1 + nota2) / 2
+    print(media)
+ 
+calcular_media(10, 9)'''
+
+'''def somar(a, b):
+    return a + b
+
+args = [3,5]
+resultado = somar(*args)
+print(resultado)
+
+kwargs = {'a': 3, 'b': 5}
+resultado = somar(**kwargs)
+print(resultado)'''
+
+'''def somar(a = 2, b = 3):
+    return a + b
+
+resultado = somar(5, 10)
+print(resultado)'''
+
+'''def altera_a(a):
+    a = a + 1
+    print(a)
+
+a = 2
+altera_a(a)
+print(a)
+'''
+
+'''def altera_lista(lista):
+    lista.append(2)
+    lista.append(5)
+    print(lista)
+
+lista = [1, 7, 8, 3]
+altera_lista(lista[:])
+
+print(lista)'''
+
+'''def soma_total(*numeros):
+    return sum(numeros)
+
+print(soma_total(1, 2, 3))
+print(soma_total(10, 20, 30, 40))
+print(soma_total())'''
+
+def exibir_informacoes(**informacoes):
+    for chave, valor in informacoes.items():
+        print(f'{chave}: {valor}')
+
+exibir_informacoes(nomes='Ana', idade=25, cidade='São Paulo')
+exibir_informacoes(produto='Notebook', preco=2500.00, marca='Dell')
+
