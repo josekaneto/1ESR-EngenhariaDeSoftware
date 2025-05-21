87d1a008d0f6bcef89bfb5b602fd457be9700e6d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-20 22:14:50 -0300

    API e métodos GET, POST

---
+++ Python/fast_api_project/controllers/example_controller.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from services.example_service import get_data, save_data
+
+router = APIRouter()
+
+class ExampleRequest(BaseModel):
+    name: str
+    age: int
+
+@router.get("/example")
+async def get_example():
+    return {
+        "status": "success",
+        "data": get_data()
+    }
+
+@router.post("/example")
+async def post_example(data: ExampleRequest):
+    processed = save_data(data.dict())
+    return {
+        "status": "success",
+        "message": "Data received and processed",
+        "processed": processed
+    }
+
+@router.delete("/example")
+async def delete_example(data: ExampleRequest):
+    return {
+        "status": "User successefully deleted",
+        "message": "Data received and processed",
+    }
