6c4db910d6a272964df0fb48a22c5c13d79cb9c7
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-08 21:25:43 -0300

    aula 08/08 Webdev

---
+++ webDevelopment/React/aula0808/react-com-build/app/page.js
@@ -0,0 +1,12 @@
+import { Hello } from "./Hello";
+import HelloComProps from "./HelloComProps";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello com Next!</h1>
+      <Hello/>
+      <HelloComProps name="José"/>
+    </>
+  );
+}
