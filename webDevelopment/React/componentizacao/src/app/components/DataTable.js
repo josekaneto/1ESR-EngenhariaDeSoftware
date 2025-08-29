a7e57ab1b23e01daf0169ce2682740a86c148109
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-29 20:51:17 -0300

    Compenentização

---
+++ webDevelopment/React/componentizacao/src/app/components/DataTable.js
@@ -0,0 +1,48 @@
+export default function DataTable({ title, columns, data }) {
+  return (
+    <div className="bg-white rounded-lg shadow">
+      <div className="px-6 py-4 border-b border-gray-200">
+        <h2 className="text-lg font-medium text-gray-900">{title}</h2>
+      </div>
+      <div className="overflow-hidden">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            <tr>
+              {columns.map((column) => (
+                <th
+                  key={column}
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  {column}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            {data.map((row) => (
+              <tr key={row.id}>
+                {Object.values(row).map((cell, cellIndex) => (
+                  <td key={cellIndex} className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                    {cell}
+                  </td>
+                ))}
+              </tr>
+            ))}
+            {/* {recentOrders.map((order) => (
+              <tr key={order.id}>
+ 
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {order.customer}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                  {order.amount}
+                </td>
+                
+              </tr>
+            ))} */}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
---
+++ webDevelopment/React/componentizacao/src/app/components/StatsGrid.js
@@ -0,0 +1,11 @@
+import StatCard from "./StatCard";
+
+export default function StatsGrid({stats}) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
+      {stats.map((stat, index) => (
+        <StatCard key={index} {...stat}/>
+      ))}
+    </div>
+  );
+}
